e305a328535c32a47ed18e73b940aee765a640b2
Kenneth Trott <kentrottjr@gmail.com>
2018-12-18 11:38:49 -0800

    multiple items stored to local storage. delete value button removes current item from local storage.

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 $(document).ready(function(){
   console.log('jQuery loaded');
 
+  //create helper function that creates new keyname for each new entry
+  var num = 0;
+  var value = 'inputFieldValue';
+  var inputFieldValue = function() {
+      
+      // return value + (num++); 
+
+    // return function() {
+      num++
+      value = 'inputFieldValue' + num;
+      return value;
+    // }
+  } 
+    // console.log(inputFieldValue()());
+
   // write to local storage from input when button save clicked
   $('.btn-submit').on('click', function(){
-    localStorage.setItem('inputFieldValue', $('.text-entry').val());
-    var myItemInStorage = localStorage.getItem('inputFieldValue');
+    localStorage.setItem(inputFieldValue(), $('.text-entry').val());
+    var myItemInStorage = localStorage.getItem(value);
     console.log('myItemInStorage', myItemInStorage);
 
     // display the value here
@@ -14,7 +29,10 @@ $(document).ready(function(){
 
   // delete from local storage when delete button clicked
   $('.btn-delete').on('click', function(){
-    localStorage.removeItem('inputFieldValue');
+    if(num > 0){
+      num--;
+    }
+    localStorage.removeItem(value);
   });
 
 });
